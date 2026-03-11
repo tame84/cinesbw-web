abb0922a2beef24b8f1f2ab17da7ada7a5b3e97c
tame84 <146751584+tame84@users.noreply.github.com>
2026-03-11 11:48:59 +0100

    update db schema according to the new api schema

--- src/lib/server/database/schema.ts
+++ src/lib/server/database/schema.ts
@@ -13,8 +13,6 @@ export interface Backdrop {
 
 export interface Video {
 	key: string;
-	site: string;
-	type: string;
 	name: string;
 }
 
@@ -31,8 +29,8 @@ export const genresTable = pgTable('genres', {
 
 export const moviesTable = pgTable('movies', {
 	uuid: uuid('uuid').primaryKey().defaultRandom(),
-	tmdbId: integer('tmdb_id').unique(),
-	imdbId: text('imdb_id').unique(),
+	tmdbId: integer('tmdb_id'),
+	imdbId: text('imdb_id'),
 	slug: text('slug').notNull().unique(),
 	title: text('title').notNull(),
 	releaseDate: text('release_date'),
